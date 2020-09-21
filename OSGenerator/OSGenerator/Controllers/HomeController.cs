48b1045710f17c37690aec68dedf605f63430c39
JenniferGaupel <jennifergaupel@gmail.com>
2020-09-21 14:19:44 -0400

    Updated the statement model for holding the deserialized JSON

--- OSGenerator/OSGenerator/Controllers/HomeController.cs
+++ OSGenerator/OSGenerator/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using OSGenerator.Models;
 
 namespace OSGenerator.Controllers
 {
@@ -10,14 +13,24 @@ namespace OSGenerator.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            Root ominousStatement = new Root();
+            string jsonResult;
+
+            using (StreamReader streamReader = new StreamReader("C:\\dev\\OminousStatementGenerator\\OSGenerator\\OSGenerator\\Data\\statements.json"))
+            {
+                jsonResult = streamReader.ReadToEnd();
+            }
+
+            ominousStatement = JsonConvert.DeserializeObject<Root>(jsonResult);
+
+            return View(ominousStatement);
         }
 
-        public ActionResult OSGenerate()
+        public string OSGenerate()
         {
-            ViewBag.Message = "Your ominous statement here";
+            //ViewBag.Message = "Your ominous statement here";
 
-            return View();
+            return "Your ominous statement here";
         }
     }
 }
--- OSGenerator/OSGenerator/Models/StatementModel.cs
+++ OSGenerator/OSGenerator/Models/StatementModel.cs
@@ -5,7 +5,17 @@ using System.Web;
 
 namespace OSGenerator.Models
 {
-    public class StatementModel
+    public class Statement
     {
+        public List<string> firstStatement { get; set; }
+        public List<string> secondStatement { get; set; }
+        public List<string> thirdStatement { get; set; }
+        public List<string> fourthStatement { get; set; }
     }
+
+    public class Root
+    {
+        public List<Statement> statements { get; set; }
+    }
+
 }
