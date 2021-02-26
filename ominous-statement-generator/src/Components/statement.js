311965afea6b86aa7e603b2a43f775785c13ac28
JenniferGaupel <jennifergaupel@gmail.com>
2021-02-25 19:52:07 -0500

    Adding the ominous statements file

--- ominous-statement-generator/src/Components/statement.js
+++ ominous-statement-generator/src/Components/statement.js
@@ -1,5 +1,8 @@
+import statementData from '../../public/assets/statements.json';
 const Statement = () => {
+  const data = JSON.parse(statementData);
   const message = "ominous statement";
+  message += data.statements.firstStatement[1];
   return <div>{message}</div>;
 };
 
