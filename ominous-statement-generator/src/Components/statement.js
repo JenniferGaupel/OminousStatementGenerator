742dcb3a03bbf890e4ef50fd3ad7be9781e5bc9d
Jennifer Gaupel <jennifergaupel@gmail.com>
2021-02-27 10:48:11 -0500

    Imported json and displaying random statement from the json data

--- ominous-statement-generator/src/Components/statement.js
+++ ominous-statement-generator/src/Components/statement.js
@@ -1,8 +1,12 @@
-import statementData from '../../public/assets/statements.json';
+import statementData from '../assets/statements.json';
 const Statement = () => {
-  const data = JSON.parse(statementData);
-  const message = "ominous statement";
-  message += data.statements.firstStatement[1];
+  var random1 = Math.floor(Math.random() * 11);
+  var random2 = Math.floor(Math.random() * 5);;
+  var random3 = Math.floor(Math.random() * 3);;
+  var random4 = Math.floor(Math.random() * 5);;
+
+  var  message = statementData.statements.firstStatement[random1] + statementData.statements.secondStatement[random2] 
+                  + statementData.statements.thirdStatement[random3] + statementData.statements.fourthStatement[random4];
   return <div>{message}</div>;
 };
 
